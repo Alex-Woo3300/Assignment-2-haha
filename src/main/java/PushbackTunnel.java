03f2473e21c9ea7805e4eaa82d7da553ab11232b
Alex-Woo3300 <ADW148@pitt.edu>
2026-09-24 22:30:36 -0400

    Update PushbackTunnel.java

--- src/main/java/PushbackTunnel.java
+++ src/main/java/PushbackTunnel.java
@@ -1,15 +1,22 @@
+Pushbacktunnel · JAVA
 public class PushbackTunnel implements PushbackTunnelInterface {
-
+ 
     public PushbackTunnel() {
         // TODO: fill in based on autograder feedback
     }
-
+ 
     public int countInTunnel(char c) {
         // TODO: real logic once we know what this should track
         return 0;
     }
-
+ 
+    public int majorityHolder(int a, int b) {
+        // TODO: real logic once we know what this should return
+        return 0;
+    }
+ 
     // TODO: add methods required by PushbackTunnelInterface.
     // The compiler will list any missing overrides once this file
     // is in place alongside PushbackTunnelInterface.java.
 }
+ 
